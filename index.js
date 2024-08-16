f3e7d6fd10b6b6dfeea9d4718f68206f82ed94b1
chuanghiduoc <levanbaotrong03@gmail.com>
2024-08-16 11:21:25 +0700

    feat: update proxy

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
 const { handleLogin, getHistories } = require("./mainTpbank/main");
 const cors = require("cors");
 
+let accessToken = null;
+
+require('dotenv').config();
+
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -12,23 +16,41 @@ app.post("/login", async (req, res) => {
   try {
     const { username, password, deviceId } = req.body;
     const login = await handleLogin(username, password, deviceId);
-    return res.json({ accessToken: login.access_token });
+    return res.json({ accessToken: login });
   } catch (error) {
     res.json({error : error.data})
   }
 });
 
 app.post("/histories", async (req, res) => {
-  try {
-    const { accessToken, accountId, deviceId } = req.body;
+  try {    
+    const username = req.body.username || process.env.USERNAME;
+    const password = req.body.password || process.env.PASSWORD;
+    const deviceId = req.body.deviceId || process.env.DEVICE_ID;
+    const accountId = req.body.accountId || process.env.ACCOUNT_ID;
+
+    if (!username || !password || !deviceId || !accountId) {
+      return res.status(400).json({ error: "Thiếu tham số bắt buộc" });
+    }
+
+    // Nếu chưa có token hoặc token đã hết hạn thì đăng nhập lại
+    if (!accessToken) {
+      console.log('Token hết hạn hoặc chưa có, đang đăng nhập lại...');
+      const loginResponse = await handleLogin(username, password, deviceId);
+      accessToken = loginResponse.access_token;
+    }
+    console.log(accessToken);
+
+    // Lấy lịch sử giao dịch sử dụng token từ đăng nhập
     const histories = await getHistories(accessToken, accountId, deviceId);
+
     //Lấy dữ liệu ở trường creditDebitIndicator = CRDT (Cộng tiền), DBIT(Trừ tiền) 
     // const filteredTransactions = histories.transactionInfos.filter(transaction => transaction.creditDebitIndicator === 'CRDT');
     // //Lọc và loại bỏ dữ liệu có trường Category
     // const transactionsWithoutCategory = filteredTransactions.map(({ category, ...transaction }) => transaction);
     return res.json({ info: histories });
   } catch (error) {
-    res.json({ error: error.data });
+    return res.status(error.response ? error.response.status : 500).json({ error: error.message });
   }
 });
 
--- mainTpbank/main.js
+++ mainTpbank/main.js
@@ -1,17 +1,45 @@
 const axios = require("axios");
 const moment = require('moment-timezone');
+const { HttpsProxyAgent } = require('https-proxy-agent');
+
+let accessToken = null;
+let clearAccessTokenTimeout = null;
+
+//Thêm proxy vào để tránh bị block, kèm nó vào trong req (config) là được
+function getAgent() {
+  const proxy = {
+    schema: process.env.PROXY_SCHEMA,
+    ip: process.env.PROXY_IP,
+    port: process.env.PROXY_PORT,
+    username: process.env.PROXY_USERNAME,
+    password: process.env.PROXY_PASSWORD,
+  };
+
+  if (!proxy.schema || !proxy.ip || !proxy.port || !proxy.username || !proxy.password) {
+    console.log("Thiếu config proxy");
+  }
+  const proxyUrl = proxy.username && proxy.username.length > 0
+    ? `${proxy.schema}://${proxy.username}:${proxy.password}@${proxy.ip}:${proxy.port}`
+    : `${proxy.schema}://${proxy.ip}:${proxy.port}`;
+
+  const httpsAgent = new HttpsProxyAgent(proxyUrl);
+
+  // console.log("httpsAgent configuration:", httpsAgent);
+
+  return httpsAgent;
+}
+
 async function handleLogin(username, password, deviceId) {
   const data = {
     username,
     password,
-    step_2FA: "VERIFY",
     deviceId,
     transactionId: "",
   };
 
   const config = {
     headers: {
-      APP_VERSION: "2024.06.28",
+      APP_VERSION: "2024.07.12",
       Accept: "application/json, text/plain, */*",
       "Accept-Language": "vi",
       Authorization: "Bearer",
@@ -21,27 +49,34 @@ async function handleLogin(username, password, deviceId) {
       DEVICE_NAME: "Chrome",
       Origin: "https://ebank.tpb.vn",
       PLATFORM_NAME: "WEB",
-      PLATFORM_VERSION: "126",
+      PLATFORM_VERSION: "127",
       Referer: "https://ebank.tpb.vn/retail/vX/",
       SOURCE_APP: "HYDRO",
       "Sec-Fetch-Dest": "empty",
       "Sec-Fetch-Mode": "cors",
       "Sec-Fetch-Site": "same-origin",
       "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
       "sec-ch-ua":
-        '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+        '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
       "sec-ch-ua-mobile": "?0",
       "sec-ch-ua-platform": '"macOS"',
     },
   };
 
   try {
-    const response = await axios.post(
-      "https://ebank.tpb.vn/gateway/api/auth/login/v3",
-      data,
-      config
-    );
+    const response = await axios.post('https://ebank.tpb.vn/gateway/api/auth/login/v3', data, { ...config, httpsAgent: getAgent() },);
+    accessToken = response.data.access_token;
+
+    if (clearAccessTokenTimeout) {
+      clearTimeout(clearAccessTokenTimeout);
+    }
+
+    // Đặt thời gian chờ để xóa accessToken ngay trước khi hết hạn
+    clearAccessTokenTimeout = setTimeout(() => {
+      accessToken = null;
+    }, (response.data.expires_in - 10) * 1000);
+
     return response.data;
   } catch (error) {
     throw error;
@@ -49,9 +84,11 @@ async function handleLogin(username, password, deviceId) {
 }
 
 async function getHistories(token, accountId, deviceId) {
+  const days = process.env.DAYS || 30;
+
   const fromDate = moment()
     .tz('Asia/Ho_Chi_Minh')
-    .subtract(14, 'days')
+    .subtract(days, 'days')
     .format('YYYYMMDD');
   const toDate = moment().tz('Asia/Ho_Chi_Minh').format('YYYYMMDD');
 
@@ -68,7 +105,7 @@ async function getHistories(token, accountId, deviceId) {
 
   const config = {
     headers: {
-      APP_VERSION: "2024.06.28",
+      APP_VERSION: "2024.07.12",
       Accept: "application/json, text/plain, */*",
       "Accept-Language": "vi,en-US;q=0.9,en;q=0.8",
       Authorization: `Bearer ${token}`,
@@ -78,15 +115,15 @@ async function getHistories(token, accountId, deviceId) {
       DEVICE_NAME: "Chrome",
       Origin: "https://ebank.tpb.vn",
       PLATFORM_NAME: "WEB",
-      PLATFORM_VERSION: "126",
+      PLATFORM_VERSION: "127",
       SOURCE_APP: "HYDRO",
       "Sec-Fetch-Dest": "empty",
       "Sec-Fetch-Mode": "cors",
       "Sec-Fetch-Site": "same-origin",
       "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
       "sec-ch-ua":
-        '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+        '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
       "sec-ch-ua-mobile": "?0",
       "sec-ch-ua-platform": '"macOS"',
     },
@@ -96,7 +133,7 @@ async function getHistories(token, accountId, deviceId) {
     const response = await axios.post(
       "https://ebank.tpb.vn/gateway/api/smart-search-presentation-service/v2/account-transactions/find",
       data,
-      config
+      { ...config, httpsAgent: getAgent() },
     );
     return response.data;
   } catch (error) {
